75ccafc002cf140e3b514b7ecb43a15f36de5c90
Henok Kirubel <henokhm2@gmail.com>
2023-06-21 21:20:55 +0300

    Update stringLength to throw error

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const stringLength = (string) => string.length;
+const stringLength = (string) => {
+ const len = string.length;
+ if(len >= 1 && len <= 10){
+    return len;
+ }
+ throw new Error("String must be between one and ten characters long, inclusive.")
+};
 
 module.exports = { stringLength };
